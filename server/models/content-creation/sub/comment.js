e676918cb928607ea3d35408db936666ff3f6ec7
astro0725 <angelica.strong0725@gmail.com>
2024-03-23 14:33:09 -0700

    comment schema

---
+++ server/models/content-creation/sub/comment.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Types;
+const { Schema } = mongoose;
+
+const commentSchema = new Schema({
+  content: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 200,
+  },
+  author: {
+    type: ObjectId,
+    ref: 'User',
+    required: true,
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model('comment', commentSchema);
