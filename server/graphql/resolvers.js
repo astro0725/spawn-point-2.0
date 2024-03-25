a2b6a74a6b3abd20ad7f6df1b490f621f44df508
astro0725 <angelica.strong0725@gmail.com>
2024-03-25 15:20:09 -0700

    create user mutation

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -16,6 +16,26 @@ const resolvers = {
   },
 
   Mutation: {
+    // create a new user
+    addUser: async (_, { username, email, password }, context) => {
+      // check if username or email already exists
+      const existingUser = await db.User.findOne({ $or: [{ username }, { email }] });
+      if (existingUser) {
+        throw new Error('Username or email already exists.');
+      }
+      // variable for hashing password
+      const hashedPassword = await bcrypt.hash(password, saltRounds);
+      // new user saved to database & hashes password
+      const newUser = new db.User({
+        username,
+        email,
+        password: hashedPassword
+      });
+      // save user to database
+      await newUser.save();
+      const token = signToken(newUser);
+      return { token, user: newUser };
+    },
   }
 };
 
