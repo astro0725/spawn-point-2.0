c58867e571e20f2255d2c7765cebd26df7d6f285
astro0725 <angelica.strong0725@gmail.com>
2024-03-27 11:17:33 -0700

    added comments for clarity

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -8,70 +8,85 @@ const resolvers = {
     // find user by id
     userById: async (_, { id }) => {
       try {
+        // use mongoose model to find user by id
         const user = await db.User.findById(id);
         return user;
       } catch (err) {
-        console.log('Error finding user by id:' + err);
+        // log error if there is a problem finding the user
+        console.log('error finding user by id:' + err);
       }
     },
     // get all users
     allUsers: async () => {
       try {
+        // use mongoose model to find all users
         const users = await db.User.find({});
         return users;
       } catch (error) {
-        console.log('Error fetching all users:' + error);
+        // log error if there is a problem fetching all users
+        console.log('error fetching all users:' + error);
       }
     },
     // get all users with pagination
     userSort: async (_, { filter }) => {
       try {
+        // initial empty query object
         const query = {};
         if (filter) {
+          // if filter is provided, create regex queries for username and name
           const { username, name } = filter;
           if (username) query.username = new RegExp(username, 'i');
           if (name) query.name = new RegExp(name, 'i');
         }
+        // use mongoose model to find users based on query
         const users = await db.User.find(query);
-        skip((page - 1 ) * limit);
         return users;
       } catch (error) {
-        console.log('Error filtering users by search:' + error);
+        // log error if there is a problem filtering users
+        console.log('error filtering users by search:' + error);
       }
     },
     // find game by id
     gameById: async (_, { id }) => {
       try {
+        // use mongoose model to find game by id
         const game = await db.Game.findById(id);
         return game;
       } catch (error) {
-        console.log('Error finding game by id:' + error);
+        // log error if there is a problem finding the game
+        console.log('error finding game by id:' + error);
       }
     },
-    // get all users
+    // get all games
     allGames: async () => {
       try {
+        // use mongoose model to find all games
         const games = await db.Game.find({});
         return games;
       } catch (error) {
-        console.log('Error fetching all games:' + error);
+        // log error if there is a problem fetching all games
+        console.log('error fetching all games:' + error);
       }
     },
-    // get all users with pagination
+    // get all games with pagination
     gameSort: async (_, { page = 1, limit = 10, tag, keyword }) => {
       try {
+        // initial empty query object
         const query = {};
+        // if tag is provided, add to query
         if (tag) {
           query.tags = tag;
         }
+        // if keyword is provided, create a regex query for name
         if (keyword) {
           query.name = new RegExp(keyword, 'i');
         }
+        // use mongoose model to find games based on query
         const games = await db.Game.find(query);
-        skip((page - 1) * limit);
         return games;
       } catch (error) {
-        console.log('Error filtering games by search:' + error);
+        // log error if there is a problem filtering games
+        console.log('error filtering games by search:' + error);
       }
     },
   },
@@ -79,28 +94,31 @@ const resolvers = {
   Mutation: {
     // create a new user
     createUser: async (_, { username, email, password }) => {
-      // check if username already exists in the database
+      // check if username already exists
       const existingUsername = await db.User.findOne({ username });
       if (existingUsername) {
-        throw new Error('Username already exists.');
+        // throw error if username exists
+        throw new Error('username already exists.');
       }
-      // check if the email already exists in the database
+      // check if email already exists
       const existingEmail = await db.User.findOne({ email });
       if (existingEmail) {
-        throw new Error('Email already exists.');
+        // throw error if email exists
+        throw new Error('email already exists.');
       }
-      // hash the password
+      // hash the password before saving to database
       const hashedPassword = await bcrypt.hash(password, saltRounds);
-      // create a new user and save to the database
+      // create new user with hashed password
       const newUser = new db.User({
         username,
         email,
         password: hashedPassword,
       });
+      // save the new user
       await newUser.save();
       // generate a token for the new user
       const token = signToken(newUser);
-      // return the token and the user in an object that matches the AuthPayload type
+      // return the token and the user
       return {
         token,
         user: newUser,
@@ -108,71 +126,69 @@ const resolvers = {
     },
     // change email of authenticated user
     changeEmail: async (_, { userId, newEmail }, context) => {
-      // checks if the data to be changed belongs to the logged in user
+      // ensure the request comes from an authenticated user
       if (!context.user || context.user._id !== userId) {
-        throw new Error('Unauthorized');
+        throw new Error('unauthorized');
       }
+      // check if the new email is already in use
       const emailExists = await db.User.findOne({ email: newEmail });
       if (emailExists) {
-        return { success: false, message: 'Email already in use' };
+        return { success: false, message: 'email already in use' };
       }
+      // find the user by id and update their email
       const user = await db.User.findById(userId);
-      if (!user) {
-        throw new Error('User not found');
-      }
       user.email = newEmail;
       await user.save();
-      return { success: true, message: 'Email successfully changed' };
+      // confirm the email has been changed
+      return { success: true, message: 'email successfully changed' };
     },
-    // change username of authenticated user
+    // change the username of an authenticated user
     changeUsername: async (_, { userId, newUsername }, context) => {
-      // checks if the data to be changed belongs to the logged in user
+      // ensure the request comes from an authenticated user
       if (!context.user || context.user._id !== userId) {
-        throw new Error('Unauthorized');
+        throw new Error('unauthorized');
       }
+      // check if the new username is already in use
       const usernameExists = await db.User.findOne({ username: newUsername });
       if (usernameExists) {
-        return { success: false, message: 'Username already in use' };
+        return { success: false, message: 'username already in use' };
       }
+      // find the user by id and update their username
       const user = await db.User.findById(userId);
-      if (!user) {
-        throw new Error('User not found');
-      }
       user.username = newUsername;
       await user.save();
-      return { success: true, message: 'Username successfully changed' };
+      // confirm the username has been changed
+      return { success: true, message: 'username successfully changed' };
     },
-    // change password of authenticated user 
+    // change the password of an authenticated user
     changePassword: async (_, { userId, oldPassword, newPassword }, context) => {
-      // checks if the data to be changed belongs to the logged in user
+      // ensure the request comes from an authenticated user
       if (!context.user || context.user._id !== userId) {
-        return { success: false, message: 'Unauthorized' };
+        return { success: false, message: 'unauthorized' };
       }
+      // verify the old password is correct
       const user = await db.User.findById(userId);
-      if (!user) {
-        return { success: false, message: 'User not found' };
-      }
       const isMatch = await bcrypt.compare(oldPassword, user.password);
       if (!isMatch) {
-        return { success: false, message: 'Old password is incorrect' };
+        return { success: false, message: 'old password is incorrect' };
       }
+      // hash the new password and update it in the database
       const hashedPassword = await bcrypt.hash(newPassword, saltRounds);
       user.password = hashedPassword;
       await user.save();
-      return { success: true, message: 'Password successfully changed' };
+      // confirm the password has been changed
+      return { success: true, message: 'password successfully changed' };
     },
-    // delete user
+    // delete a user
     deleteUser: async (_, { userId }, context) => {
-      // checks if the data to be changed belongs to the logged in user
+      // ensure the request comes from an authenticated user
       if (!context.user || context.user._id !== userId) {
-        throw new Error('Unauthorized');
+        throw new Error('unauthorized');
       }
-      const user = await db.User.findById(userId);
-      if (!user) {
-        throw new Error('User not found');
-      }
+      // delete the user from the database
       await db.User.findByIdAndDelete(userId);
-      return { success: true, message: 'User successfully deleted' };
+      // confirm the user has been deleted
+      return { success: true, message: 'user successfully deleted' };
     },
     // create a new notification
     createNotification: async (_, { type, message, userId, relatedContentId, onModel }) => {
@@ -195,33 +211,46 @@ const resolvers = {
       }
     },
     // add game to user creation
-    addGame: async (_, { title, splashArt}) => {
+    addGame: async (_, { title, splashArt }) => {
       try {
-        const newGame = new Game ({ title, splashArt });
+        // create a new game instance
+        const newGame = new Game({ title, splashArt });
+        // save the new game to the database
         const savedGame = await newGame.save();
+        // return the saved game object
         return savedGame;
-        } catch (error) {
-        console.error('Error adding game:', error);
+      } catch (error) {
+        // log and throw any errors encountered during the game creation process
+        console.error('error adding game:', error);
         throw error;
       }
     },
-    // update game in user creation
+    
+    // update a game in user creation
     updateGame: async (_, { id, title, splashArt }) => {
       try {
+        // find a game by its id and update its title and splash art
+        // the { new: true } option returns the updated document
         const updatedGame = await Game.findByIdAndUpdate(id, { title, splashArt }, { new: true });
+        // return the updated game object
         return updatedGame;
       } catch (error) {
-        console.error('Error updating game:', error);
+        // log and throw any errors encountered during the game update process
+        console.error('error updating game:', error);
         throw error;
       }
     },
-    // delete game from user creation
+    
+    // delete a game from user creation
     deleteGame: async (_, { id }) => {
       try {
+        // find a game by its id and delete it
         const deletedGame = await Game.findByIdAndDelete(id);
+        // return the deleted game object
         return deletedGame;
       } catch (error) {
-        console.error('Error deleting game:', error);
+        // log and throw any errors encountered during the game deletion process
+        console.error('error deleting game:', error);
         throw error;
       }
     },
